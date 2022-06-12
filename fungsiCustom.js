4b8a453def324f4177d9e4de75eafaed24ae7a80
Latif Ariyadi <latifariyadi86@gmail.com>
2022-06-12 19:00:53 +0700

    firsst commit

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -7,23 +8,54 @@ let file3 = "./data3.json";
 
 // ! JANGAN DIMODIFIKASI
 let modifyFile1 = (val) => {
-  file1 = val;
+    file1 = val;
 };
 let modifyFile2 = (val) => {
-  file2 = val;
+    file2 = val;
 };
 let modifyFile3 = (val) => {
-  file3 = val;
+    file3 = val;
 };
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+    fs.readFile(file1, "utf-8", (err, data1) => {
+        if (err) {
+            return console.log("Error data 1" + err);
+        }
+
+        fs.readFile(file2, "utf-8", (err, data2) => {
+            if (err) {
+                return console.log("Error data 2" + err);
+            }
+
+            fs.readFile(file3, "utf-8", (err, data3) => {
+                const parseData1 = JSON.parse(data1);
+                const message1 = getLastWord(parseData1.message);
+
+                const parseData2 = JSON.parse(data2);
+                const message2 = getLastWord(parseData2[0].message);
+
+                const parseData3 = JSON.parse(data3);
+                const message3 = getLastWord(parseData3[0].data.message);
+
+                fnCallback(err, [message1, message2, message3]);
+            });
+        });
+    });
+};
+
+const getLastWord = (sentence) => {
+    const arrSentence = sentence.split(" ").reverse();
+
+    return arrSentence[0];
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
-  modifyFile1,
-  modifyFile2,
-  modifyFile3,
-  bacaData,
+    modifyFile1,
+    modifyFile2,
+    modifyFile3,
+    bacaData,
 };
